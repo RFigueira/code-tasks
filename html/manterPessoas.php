d1a008e402106fd065881d957f1fd5d652621d35
Rodrigo <foolha@gmail.com>
2016-07-04 21:21:32 -0300

    [#253] Modal de editar não abre

--- html/manterPessoas.php
+++ html/manterPessoas.php
@@ -5,6 +5,8 @@ ini_set('default_charset', 'UTF-8');
 ini_set('display_errors', true);
 error_reporting(E_ERROR | E_WARNING);
 error_reporting(E_ALL);
+include 'index.php';
+
 
   require_once '../service/PessoaService.class.php';
   require_once '../model/Pessoa.class.php';
@@ -13,18 +15,20 @@ error_reporting(E_ALL);
 
 
 
+  $e='';
   $msg = '';
   $teste ='';
   $pessoaService = new PessoaService;
   $pessoa = new Pessoa();
-
   if(isset($_GET["id"])) {
     if(isset($_GET["nome"])) {
       $pessoa = $pessoaService->findById($_GET["id"]);
+      $e = '$("#myModal").modal("show");';
+
     }
     else {
       $pessoaService->delete($_GET["id"]);
-      header("location: manterPessoas.php");
+      //header("location: manterPessoas.php");
     }
   }
   else {
@@ -42,6 +46,7 @@ error_reporting(E_ALL);
 
     if($msg == null){
       $pessoaService->salvar($pessoa);
+
     }
   }
 }
@@ -49,104 +54,7 @@ error_reporting(E_ALL);
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>SB Admin - Bootstrap Admin Template</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="outros/startbootstrap-sb-admin-1.0.4/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="outros/startbootstrap-sb-admin-1.0.4/css/sb-admin.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="outros/startbootstrap-sb-admin-1.0.4/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- Meus estilos -->
-    <link href="css/estilo.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
-<body>
-
-    <div id="wrapper">
-        <!-- Navigation -->
-        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.php">codePampa</a>
-            </div>
-            <!-- Top Menu Items -->
-            <ul class="nav navbar-right top-nav">
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-user"></i> Tainã Milano <b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a href="#"><i class="glyphicon glyphicon-user"></i> Meu perfil</a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="#"><i class="glyphicon glyphicon-off "></i> Sair</a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-                <ul class="nav navbar-nav side-nav">
-                    <li>
-                        <a href="index.php"><i class="glyphicon glyphicon-dashboard"></i> Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="manterPessoas.php"><i class="glyphicon glyphicon-user"></i> Pessoas</a>
-                    </li>
-                    <li>
-                        <a href="javascript:;" data-toggle="collapse" data-target="#projetos">
-                            <i class="glyphicon glyphicon-folder-open"></i> Projetos <i class="glyphicon glyphicon-chevron-down"></i>
-                        </a>
-                        <ul id="projetos" class="nav nav-second-level collapse">
-                            <li>
-                                <a href="manterProjetos.php"><i class="glyphicon glyphicon-plus"></i> Novo projeto</a>
-                            </li>
-                            <li>
-                                <a href="javascript:;" data-toggle="collapse" data-target="#tarefas">
-                                    <i class="glyphicon glyphicon-th-list"></i> Projeto 1 <i class="glyphicon glyphicon-chevron-down"></i>
-                                </a>
-                                <ul id="tarefas" class="nav nav-third-level collapse">
-                                    <li>
-                                        <a href="manterTarefas.html"><i class="glyphicon glyphicon-minus ml-15"></i> Tarefa 1</a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><i class="glyphicon glyphicon-minus ml-15"></i> Tarefa 2</a>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </nav>
         <div id="page-wrapper">
             <div class="container-fluid">
                 <!-- Page Heading -->
@@ -165,7 +73,7 @@ error_reporting(E_ALL);
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="col-lg-6">
-                            <button  type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal"><i class="glyphicon glyphicon-check"></i> Novo cadastro</button>
+                            <button onclick="newPessoa();" type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal"><i class="glyphicon glyphicon-check"></i> Novo cadastro</button>
                         </div>
                         <div class="col-lg-6">
                             <div class="col-lg-offset-6">
@@ -236,6 +144,7 @@ error_reporting(E_ALL);
             </div>
             <!-- /.container-fluid -->
             <!-- Modal -->
+
             <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
@@ -243,8 +152,8 @@ error_reporting(E_ALL);
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                             <h4 class="modal-title" id="myModalLabel"> Cadastro</h4>
                         </div>
-                        <form action="manterPessoas.php" method="post">
-                          <input type="hidden" name="txt-id" value="<?php echo $pessoa->id ?>" />
+                        <form id="form" action="manterPessoas.php" method="post">
+                          <input id="txt-id" type="hidden" name="txt-id" value="<?php echo $pessoa->id ?>" />
                           <div class="modal-body">
                             <div class="row">
                                 <div class="col-lg-12">
@@ -254,14 +163,14 @@ error_reporting(E_ALL);
                                             <button class="btn btn-default" type="button"><i class="glyphicon glyphicon-user"></i>
                                             </button>
                                         </span>
-                                        <input type="text" class="form-control" name="txt-nome" value="<?php echo $pessoa->nome?>" >
+                                        <input id="txt-nome" type="text" class="form-control" name="txt-nome" value="<?php echo $pessoa->nome?>" >
                                     </div>
                                     <div class="form-group input-group">
                                         <span class="input-group-btn">
                                             <button class="btn btn-default" type="button"><i class="glyphicon glyphicon-envelope"></i>
                                             </button>
                                         </span>
-                                        <input type="text" class="form-control" name="txt-email" value="<?php echo $pessoa->email?>" >
+                                        <input id="txt-email" type="text" class="form-control" name="txt-email" value="<?php echo $pessoa->email?>" >
                                     </div>
                                 </div>
                             </div>
@@ -273,7 +182,7 @@ error_reporting(E_ALL);
                                             <button class="btn btn-default" type="button"><i class="glyphicon glyphicon-asterisk"></i>
                                             </button>
                                         </span>
-                                        <input type="password" class="form-control" name="txt-senha" value="<?php echo $pessoa->senha?>" >
+                                        <input id="txt-senha" type="password" class="form-control" name="txt-senha" value="<?php echo $pessoa->senha?>" >
                                     </div>
                                 </div>
                                 <div class="col-lg-6">
@@ -283,7 +192,7 @@ error_reporting(E_ALL);
                                             <button class="btn btn-default" type="button"><i class="glyphicon glyphicon-asterisk"></i>
                                             </button>
                                         </span>
-                                        <input type="password" class="form-control" name="txt-senha-confirmacao" >
+                                        <input id="txt-senha-confirmacao" type="password" class="form-control" name="txt-senha-confirmacao" >
                                     </div>
                                 </div>
                             </div>
@@ -294,19 +203,22 @@ error_reporting(E_ALL);
                                 Salvar
                             </button>
                           </div>
-                        </forlvar
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
         <!-- /#page-wrapper -->
     </div>
-    <!-- /#wrapper -->
-    <!-- jQuery -->
-    <script src="outros/startbootstrap-sb-admin-1.0.4/js/jquery.js"></script>
-    <!-- Bootstrap Core JavaScript -->
-    <script src="outros/startbootstrap-sb-admin-1.0.4/js/bootstrap.min.js"></script>
-
 </body>
-
+<script><?php echo $e ?>;</script>
+<script>
+  function newPessoa() {
+    document.getElementById("txt-nome").value ='';
+    document.getElementById("txt-id").value ='';
+    document.getElementById("txt-email").value ='';
+    document.getElementById("txt-senha").value ='';
+    document.getElementById("txt-senha-confirmacao").value ='';
+  };
+</script>
 </html>
